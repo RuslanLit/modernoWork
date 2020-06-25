00fc028752849c381e311550d037b68cf6693ccb
RuslanLit <arslanlitvin@gmail.com>
2020-06-25 21:03:35 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,17 @@ $(function() {
 
 $(".rate-star").rateYo({
     rating: 5,
-    starWidth: "12px"
+    starWidth: "12px",
+    readOnly: true
+  });
+
+  $('.product-slider__inner').slick({
+    dots:true,
+    arrows:false,
+    slidesToShow: 4,
+    slidesToScroll:4,
+    infinite: true
+
   });
 
 var mixer = mixitup('.products__inner-box');
